b8c7490dba314c416eca53243f62298279bf92a3
guihlr <guilhermehlr9@gmail.com>
2019-03-21 14:50:08 -0300

    mais testes

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 def soma(a, b):
   return a + b
+
+def multi(x, y):
+  return x / y
--- teste.py
+++ teste.py
@@ -1,5 +1,9 @@
 import pytest
 from main import soma
+from main import multi
 
 def test_soma():
   assert soma(6, 4) == 10
+
+def test_multi():
+  assert multi(2, 4) == 8
